f14cc8e5bad92b37cbf440d87ddf720ff11708d3
Nahuel <ariasnahuel8@gmail.com>
2025-07-22 18:24:23 -0300

    feat: :sparkles: SEO y Accesibilidad con React Helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import { HelmetProvider } from 'react-helmet-async'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-  integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-  crossorigin="anonymous"
-  referrerpolicy="no-referrer"
-/>
-    <App />
-  </StrictMode>,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 )
